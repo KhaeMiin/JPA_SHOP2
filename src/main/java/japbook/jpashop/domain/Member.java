81b53711ed542095ef063b852aa96291bf0d4f0c
minMin <goals1005@naver.com>
2022-05-26 10:13:54 +0900

    엔티티 클래스 개발

---
+++ src/main/java/japbook/jpashop/domain/Member.java
@@ -0,0 +1,26 @@
+package japbook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") //누구에 의해서 맵핑 되는지!(읽기전용이 됩니다!)
+    private List<Order> orders = new ArrayList<>();
+}
---
+++ src/main/java/japbook/jpashop/domain/OrderItem.java
@@ -0,0 +1,29 @@
+package japbook.jpashop.domain;
+
+import japbook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne //하나의 아이템에서 여러개의 주문이 가능
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice; //주문가격
+    private int count; //주문 수량
+
+
+}
