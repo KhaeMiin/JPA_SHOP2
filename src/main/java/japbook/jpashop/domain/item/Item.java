b68189071c6198c73a2807c7ae105763d8000a53
minMin <goals1005@naver.com>
2022-05-28 23:08:42 +0900

    상품 도메인 개발

--- src/main/java/japbook/jpashop/domain/item/Item.java
+++ src/main/java/japbook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package japbook.jpashop.domain.item;
 
 import japbook.jpashop.domain.Category;
+import japbook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,23 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    //==비즈니스 로직==//
+
+    /**
+     * stock 재고 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock"); // 재고 관련 Exception 만듦
+        }
+        this.stockQuantity = restStock;
+    }
 }
