d81bc1cf08e85ff483c3ba8e520ae4be2a6fa2f6
haemin <goals1005@naver.com>
2022-08-09 23:21:14 +0900

    에엔티티 직접 노ì출: 컬렉션 최적화

---
+++ src/main/java/japbook/jpashop/api/OrderApiController.java
@@ -0,0 +1,30 @@
+package japbook.jpashop.api;
+
+import japbook.jpashop.domain.Order;
+import japbook.jpashop.domain.OrderItem;
+import japbook.jpashop.repository.OrderRepository;
+import japbook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> orderV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+}
